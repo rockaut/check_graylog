9069a565c46442456c4e9635fd18f9b04726160d
Markus Fischbacher <fischbacher.markus@gmail.com>
2017-02-18 10:08:21 +0100

    rewritten flags

--- main.go
+++ main.go
@@ -1,28 +1,25 @@
 package main
 
 import (
+    "errors"
+    "flag"
     "fmt"
     "github.com/rockaut/check_graylog/pkg/graylog"
     "os"
     //"strconv"
 )
 
-var agentflags graylog.AgentFlags
 var exitCode int
 var agent graylog.Agent
 
 func init() {
-    agentflags := graylog.AgentFlags{}
-    agentflags.Initialize()
 
-    err := agentflags.Parse()
+    err := initializeFlags()
     if err != nil {
-        agentflags.Usage()
+        flag.Usage()
         exitCode = 1
         os.Exit(exitCode)
     }
-
-    fmt.Printf("mode: %v (%v)\n", agentflags.Mode, int(agentflags.Mode))
 
     agent = graylog.Agent{
         Host:     "127.0.0.1",
@@ -46,4 +43,26 @@ func main() {
     fmt.Print(res)
 
     os.Exit(exitCode)
+}
+
+func initializeFlags() error {
+    flagMode := flag.String("mode", "cmk", "Agent Mode {cmk|zbx}. cmk = Check_MK, zbx = Zabbix")
+    flagHost := flag.String("host", "", "Graylog node to fetch data")
+
+    flag.Parse()
+
+    switch *flagMode {
+    case "cmk":
+    case "zbx":
+        //pass
+    default:
+        return errors.New("Unknown agentmode given")
+    }
+
+    switch *flagHost {
+    case "":
+        return errors.New("a graylog host has to be defined")
+    }
+
+    return nil
 }
